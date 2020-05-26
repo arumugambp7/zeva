afd65f104f31672826e5a9139cc2904d251e9970
Priya1192 <connect.piya@gmail.com>
2020-05-26 23:15:01 +0530

    migration fix

--- zeva/main_app/admin.py
+++ zeva/main_app/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Menu,Collection,Client,Subscriber
+# from .models import Menu,Collection,Client,Subscriber
 
 
 # Register your models here.
-class MenuAdmin(admin.ModelAdmin):
-    list_display = ['title',]
-
-admin.site.register(Menu, MenuAdmin)
-admin.site.register(Collection)
-admin.site.register(Client)
-admin.site.register(Subscriber)
+# class MenuAdmin(admin.ModelAdmin):
+#     list_display = ['title',]
+#
+# admin.site.register(Menu, MenuAdmin)
+# admin.site.register(Collection)
+# admin.site.register(Client)
+# admin.site.register(Subscriber)
--- zeva/main_app/models.py
+++ zeva/main_app/models.py
@@ -5,45 +5,45 @@ import cloudinary
 
 # Create your models here.
 
-class Menu(models.Model):
-     category = models.CharField(max_length=3,primary_key=True)
-     image = CloudinaryField('image')
-     title = models.CharField(max_length=25)
-     class meta:
-          db_table = 'Menu'
-     def __str__(self):
-          return self.title
-
-
-
-class Collection(models.Model):
-     name = models.CharField(max_length=6)
-     image = CloudinaryField('image')
-     menu = models.ManyToManyField(Menu)
-     class meta:
-          db_table = 'Collection'
-     def __str__(self):
-          return self.name
-
-
-class Client(models.Model):
-     name = models.CharField(max_length=25,blank=True)
-     image = CloudinaryField('image')
-     location = models.CharField(max_length=25,blank=True)
-     contact = models.CharField(blank=True,max_length=10)
-     email = models.EmailField(blank=True)
-     menu = models.ManyToManyField(Menu,blank=True)
-     class meta:
-          db_table = 'Client'
-     def __str__(self):
-          return self.name
-
-class Subscriber(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.EmailField()
-    message = models.TextField(blank=True)
-    def __str__(self):
-        return self.name
+# class Menu(models.Model):
+#      category = models.CharField(max_length=3,primary_key=True)
+#      image = CloudinaryField('image')
+#      title = models.CharField(max_length=25)
+#      class meta:
+#           db_table = 'Menu'
+#      def __str__(self):
+#           return self.title
+#
+#
+#
+# class Collection(models.Model):
+#      name = models.CharField(max_length=6)
+#      image = CloudinaryField('image')
+#      menu = models.ManyToManyField(Menu)
+#      class meta:
+#           db_table = 'Collection'
+#      def __str__(self):
+#           return self.name
+#
+#
+# class Client(models.Model):
+#      name = models.CharField(max_length=25,blank=True)
+#      image = CloudinaryField('image')
+#      location = models.CharField(max_length=25,blank=True)
+#      contact = models.CharField(blank=True,max_length=10)
+#      email = models.EmailField(blank=True)
+#      menu = models.ManyToManyField(Menu,blank=True)
+#      class meta:
+#           db_table = 'Client'
+#      def __str__(self):
+#           return self.name
+#
+# class Subscriber(models.Model):
+#     name = models.CharField(max_length=20)
+#     email = models.EmailField()
+#     message = models.TextField(blank=True)
+#     def __str__(self):
+#         return self.name
 
 
 
