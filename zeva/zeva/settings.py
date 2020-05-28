0e4328a9d5cd71f8302f2258bb91c5ab079c993d
Priya1192 <connect.piya@gmail.com>
2020-05-28 14:14:29 +0530

    SSL setting modified

--- zeva/zeva/settings.py
+++ zeva/zeva/settings.py
@@ -138,7 +138,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
